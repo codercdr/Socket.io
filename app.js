8865bde4a229dbeb9ec909a536126246a43f524b
codercdr <codercdr@gmail.com>
2017-03-05 19:54:17 +0300

    Update io 1

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+	
+server.listen(3000);
+
+app.get("/",function(req,res){
+	res.sendfile(__dirname + '/index.html');	
+});
+	
+	
+io.sockets.on('connection',function(socket){
+	console.log('Bağlantı kuruldu...');
+	socket.on('send message',function(data,callback){
+		console.log('Mesaj alındı:' + data);
+		io.sockets.emit('new message',data);
+		
+		callback(10);
+	});
+	
+}); 
